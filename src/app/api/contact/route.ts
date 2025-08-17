c4a067559dd3860b04b94f8057bef779f65fb073
Matheus Chimendes <chimendes2013@outlook.com>
2025-08-17 18:12:18 -0300

    ajuste api

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      port: Number(process.env.EMAIL_PORT),
       secure: true,
       auth: {
         user: process.env.EMAIL_USER,
