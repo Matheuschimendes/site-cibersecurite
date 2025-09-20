fb3fde270015012a4ffda426911b15634a5e0885
Matheus Chimendes <chimendes2013@outlook.com>
2025-09-19 23:11:29 -0300

    ajuste de caminhos Contato

--- src/app/[locale]/[service]/page.tsx
+++ src/app/[locale]/[service]/page.tsx
@@ -6,8 +6,9 @@ import SectionSevice from "./_components/sectionSevice";
 import { SectionServiceSecond } from "./_components/sectionSeviceSecond";
 import { AboutService } from "./_components/aboutService";
 import { SectionBusiness } from "./_components/serviceBusiness";
+import { Contato } from "@/app/sections/contato";
 import FooterPage from "../../sections/footer";
-import { Contato } from "../../sections/contato";
+
 
 export default function ServicePage() {
   const _ = useParams();
